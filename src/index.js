09c9075835ae277e65e40830e1dbf12274709dda
Furb <jthabo@gmail.com>
2023-12-06 13:25:34 +0100

    mass

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/* MainNavbar */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const mainNav = document.querySelector("#main__links");
+  const navToggle = document.querySelector(".nav__toggle");
+  const menuIcon = document.querySelector(".menu__icon");
+  const overlay = document.querySelector(".overlay");
+
+  navToggle.addEventListener("click", () => {
+    const visibillity = mainNav.getAttribute("data-visible");
+
+    if (visibillity === "false") {
+      mainNav.setAttribute("data-visible", true);
+      navToggle.setAttribute("aria-expanded", true);
+      menuIcon.textContent = "close";
+      overlay.style.display = "block";
+    } else if (visibillity === "true") {
+      mainNav.setAttribute("data-visible", false);
+      navToggle.setAttribute("aria-expanded", false);
+      menuIcon.textContent = "menu";
+      overlay.style.display = "none";
+    }
+  });
+});
